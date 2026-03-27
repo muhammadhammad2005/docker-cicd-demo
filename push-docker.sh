edc89a4a3fe2df76a8dd288162810c318722b408
Muhammad Hammad <youyt.manager@gmail.com>
2026-03-27 15:14:26 +0500

    Add Docker login step before pushing images

--- push-docker.sh
+++ push-docker.sh
@@ -5,7 +5,10 @@ IMAGE_NAME="hammad2005/docker-cicd-demo"
 BUILD_NUMBER=${BUILD_NUMBER:-latest}
 GIT_COMMIT=${GIT_COMMIT:-$(git rev-parse HEAD)}
 
-echo "Pushing Docker images to Docker Hub..."
+echo "Logging into Docker Hub..."
+echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
+
+echo "Pushing Docker images..."
 docker push ${IMAGE_NAME}:${BUILD_NUMBER}
 docker push ${IMAGE_NAME}:latest
 docker push ${IMAGE_NAME}:${GIT_COMMIT:0:8}
